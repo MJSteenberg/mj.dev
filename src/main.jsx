ffd09c4241fdcb157f8d03b8b6fa7390e6b4a87f
MJ Steenberg <mjsteenberg01@icloud.com>
2023-03-12 15:02:37 +0200

    nowadays

--- src/main.jsx
+++ src/main.jsx
@@ -9,15 +9,9 @@ import {
 import App from './App'
 import './index.css'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <App />
-  // </React.StrictMode>,
+  </React.StrictMode>
 )
