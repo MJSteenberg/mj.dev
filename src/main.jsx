022f3b4a5184bb6fb33347ee23457a305976186c
MJ Steenberg <mjsteenberg01@icloud.com>
2023-03-12 15:20:12 +0200

    wow

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
 
 
-import App from './App';
-import './index.css';
+// import App from './App';
+// import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
+
+
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <BrowserRouter>
+    {
+      <App />
+    }
+
+    
+  </BrowserRouter>
+);
