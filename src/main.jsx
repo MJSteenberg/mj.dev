f6498184901aed24141ba425ea01cd1937211415
MJ Steenberg <mjsteenberg01@icloud.com>
2023-03-12 14:53:22 +0200

    wot

--- src/main.jsx
+++ src/main.jsx
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  // </React.StrictMode>,
 )
