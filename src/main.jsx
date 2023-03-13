e2844c99151930bc4e0c0bb5f75008121d7012a7
MJ Steenberg <mjsteenberg01@icloud.com>
2023-03-14 00:20:41 +0200

    deployed

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import {
-//   createBrowserRouter,
-//   RouterProvider,
-// } from "react-router-dom";
-
-
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <App />
-  // </React.StrictMode>
-)
-
-
-// import * as React from "react";
-// import { createRoot } from "react-dom/client";
-// import { BrowserRouter } from "react-router-dom";
-
-// const root = createRoot(document.getElementById("root"));
-
-// root.render(
-//   <BrowserRouter>
-    
-//       <App />
-    
-
-    
-//   </BrowserRouter>
-// );
+  </React.StrictMode>
+);
